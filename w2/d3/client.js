963bc224af9af9ea994bd8cc032dabe470251771
berkererol <berkererol@gmail.com>
2019-08-28 21:25:33 +0000

    Move upward

--- w2/d3/client.js
+++ w2/d3/client.js
@@ -1,4 +1,7 @@
-onst connect = function() {
+const net = require('net');
+
+
+const connect = function() {
   const conn = net.createConnection({ 
     host: '192.168.0.102',
     port: 50541
@@ -6,13 +9,19 @@ onst connect = function() {
   // interpret incoming data as text
   conn.setEncoding('utf8'); 
 
-  conn.on('data', (data) => {
-    console.log('Server says: ', data);
-  });
-  conn.on('connect', () => {
-    conn.write('Hello from client!');
-  });
+  conn.on('data', (data) => { console.log('Server says: ', data )});
+
+  conn.on('data', (msg) => { console.log(msg) });
+
+  conn.on('connect', () => { conn.write('Hello from client!'); });
+
+  conn.on('connect', () => { console.log('Connection is established') });
+
+  conn.on('connect', () => { conn.write('Name: BRK') });
+
+  conn.on('connect', () => { conn.write('Move: up') });
+
   return conn;
 }
 
-module.exports = connect;
+module.exports = { connect }
